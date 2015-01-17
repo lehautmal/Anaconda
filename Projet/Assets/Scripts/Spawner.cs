7d743bd7841653876275037a966337f8542a918a
unknown <xpro@336-a3.nad.qc.ca>
2015-01-17 15:25:12 -0500

    avec building

--- Projet/Assets/Scripts/Spawner.cs
+++ Projet/Assets/Scripts/Spawner.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using UnityEngine;
 using System.Collections;
 
@@ -23,35 +23,4 @@ public class Spawner : MonoBehaviour
 		{
 				ClonedFallingObject = MonoBehaviour.Instantiate (FallingObject, this.transform.position, this.transform.rotation) as GameObject;
 		}
-		
-
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class Spawner : MonoBehaviour {
-
-
-	public GameObject test;
-	private GameObject test2;
-
-
-	private float NextSpawn = 0;
-	public int SpawnRate;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if (NextSpawn < Time.time) 
-		{
-			test2 = MonoBehaviour.Instantiate (test, this.transform.position, this.transform.rotation) as GameObject;
-			NextSpawn += SpawnRate;
-		}
-	}
->>>>>>> e9bb84fc4c18470459e95f78ef5c41fd05f5647f
 }
